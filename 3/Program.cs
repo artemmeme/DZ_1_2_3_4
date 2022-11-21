375ad7dad7480a7c7a5c51587f838e06101e23ba
Артем Шустов <artemmeme2017@yandex.ru>
2022-11-21 15:30:40 +0300

    Написал программу для определения четности числа

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 ==0)
+{
+   Console.WriteLine("Число является четным");
+} 
+else 
+{
+   Console.WriteLine("Число является нечетным");
+} 
