ca07cbfc847c5671be7201b7a1d438699604e92d
Артем Шустов <artemmeme2017@yandex.ru>
2022-11-21 15:31:59 +0300

    Написал программу для вывода четных чисел от 1 до N

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2;
+num2 = 1;
+
+while (num2<num1)
+{
+    if (num2 % 2 ==0)
+    {
+           Console.Write($"{num2} ");
+    }
+    num2++;
+}
