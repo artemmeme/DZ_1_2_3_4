3c457fa9c0be715185fee5d696d17632c043daff
Артем Шустов <artemmeme2017@yandex.ru>
2022-11-21 15:29:02 +0300

    Написал программу для нахождения максимального числа из двух чисел

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max;
+max = num1;
+
+if (num1>num2)
+{
+    max = num1;
+    Console.WriteLine("Большее число");
+    Console.Write($"{max} ");
+} 
+else 
+{
+    max = num2;
+    Console.WriteLine("Большее число");
+    Console.Write($"{max} ");
+} 
+Console.WriteLine();
+
--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max;
+max = 0;
+
+if (num1>max)
+{
+    max = num1;
+} 
+
+if (num2>max)
+{
+    max = num2;
+} 
+
+if (num3>max)
+{
+    max = num3;
+} 
+Console.WriteLine("Большее число");
+    Console.Write($"{max} ");
